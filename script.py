e08e15ea12c2a417e99e9c1c2a95ef08922f23d0
rjboer <roelofjanboer@resato.com>
2016-07-05 10:26:32 +0200

    Update script.py
    cleanup

--- script.py
+++ script.py
@@ -4,14 +4,6 @@ import sys
 import string
 import re
 
-#This script is to controll a network directory, coupled to a document managment system
-#It renumbers all files (and subdirectories) with a numbered file path in a string e.g.:
-#
-#100 (or something else) base dir
-#100.1 sub dir
-#    100.1 file in subdir 1
-#100.2 subdir
-#100 file in base dir
 clear()
 startpath = "/home/bi/Desktop/proeftuin"
 #teststring1 = re.compile('(\d+(\.| |\_))+')
@@ -23,21 +15,11 @@ startpath = "/home/bi/Desktop/proeftuin"
 #teststring7 = re.compile('(\d\d+)')
 teststr7 = re.compile('([^\d\W])')
 dnumberlist = []
-#pastl1=[]
-#pastl2=[]
-#pastl3=[]
-#pastl4=[]
-#faill1=[]
-#faill2=[]
-#faill3=[]
-#faill4=[]
-#faill12=[]
-#faill22=[]
-#blist=[]
-#filenamels=[]
+
 debug=False
 files=True
 folders=True
+gitignore=True
 #
 #def rename(dir, pattern, titlePattern):
 #    for pathAndFilename in glob.iglob(os.path.join(dir, pattern)):
@@ -55,13 +37,6 @@ folders=True
 
 
 
-
-def printvars(var1,var2):
-    print("root dir en subdir respectively")
-    print("############################################")
-    print var1
-    print var2
-    print("############################################")
 
 def urify(s):
      # verwijder alle niet letters (alles anders dan nummers en letters)
@@ -136,6 +111,9 @@ def main(startpath):
     tobe, oldname, newname=[], [], []
     maxd = int
     for dirname, subdirList, filenames in os.walk(startpath):
+        if (gitignore==True and ('.git' in dirname)):
+            dirname.remove('.git')
+            #don't go into git dirs
         if (folders==True):
             if (dirname == startpath or (len(subdirList)==0 and len(filenames)==0)):
                 continue
@@ -173,87 +151,10 @@ def test3er():
         for fname in fileList:
             print('\t%s' % fname)
     return()
-#print subdirList
-#print dirName
-#print fileList
 
 main(startpath)
 print("finished, kill")
 exit()
 
-#
-#def old():
-#    i=0
-#    for dirname, subdirList, filenames in os.walk(startpath):
-#        i+=1 
-#        #skip the source directory (do not rename this) 
-#        if dirname == startpath:
-#            continue
-#        var1 = os.path.basename(dirname)
-#        print(" condition the pathname")
-#        compbasecode, basecode, var4 = mangler(var1, True)
-#    #    filenamels.append(var4)
-#    #    for base in basecode:
-#     #       compbasecode = '%s.%s' % (compbasecode, base)
-#        #print (basecode)
-#        lensdl = len(subdirList)
-#        x=0
-#        for subdir in subdirList:
-#            var2,var4=mangler(subdir, False)
-#            var3=list(var2)
-#            var3.pop()
-#            var5=urify(var4)
-#            if var3==basecode:
-#                print ("test last element")
-#                dnumberlist.append(int(var2[-1]))
-#                if var5 != var4:
-#                    faill1.append(subdir)
-#                    faill3.append(var5)
-#                    faill2.append(os.path.join(dirname,subdir))
-#                    faill4.append(x)
-#                else:
-#                    continue
-#            else:
-#                faill1.append(subdir)
-#                faill3.append(var4)
-#                faill2.append(os.path.join(dirname,subdir))
-#                faill4.append(x)
-#            x+=1
-#        maxd=max(dnumberlist)
-#        y=0
-#        for y in range(len(faill1)):
-#            maxd+=1
-#            test = '%s.%s_%s' % (compbasecode, maxd, urify(faill3[y]))
-#            test2=os.path.join(dirname,test)
-#            faill12.append(test)
-#            faill22.append(test2)
-#        changeur(faill2,faill22)
-#    return()
-#    printvars(dirname, subdirList)
-#        
-#    if len(var1) <=3:
-#        print("korte pathname")
-#        var2 = re.match(teststring7,var1)
-#        if var2 is not None:
-#            print("alleen nummers, te korte pathname negeren")
-#            continue
-#    else:
-#        print(var1)
-#        print(dirname.count(os.sep))
-#        print( dirname.count(os.sep) - startlevel)
-#        currentlevel.append(dirname.count(os.sep) - startlevel)
-#        #regmatch(var1, teststring4)
-#        dirnamelist.append(var1)
-#        pathlist.append(dirname)
-#        
-#        #print(os.path.join(dirname, subdirname))
-#        # print path to all filenames.
-#        #for filename in filenames:
-#        #    print(os.path.join(dirname, filename))
-#        #   Advanced usage:
-#        # editing the 'dirnames' list will stop os.walk() from recursing into there.
-#        if '.git' in dirname:
-#            # don't go into any .git directories.
-#            dirname.remove('.git')
-#
-#
+
+
