05b6fbf86ca8a62e146499eca8bba3d04fb019bb
rjboer <roelofjanboer@resato.com>
2016-07-06 16:59:07 +0200

    Update script.py
    update

--- script.py
+++ script.py
@@ -6,45 +6,45 @@ import time
 
 startpath = "/home/bi/Desktop/test"
 
-pattern1 = re.compile(r"[^\d\W]")
-pattern2 = re.compile(r"[^\w^\.\s]")
+pattern1 = re.compile(r"[^\d\W]")# compiled regex string
+pattern2 = re.compile(r"[^\w^\.^\-\s]")
 pattern3 = re.compile(r"\s+")
-dnumberlist = []
-#cannot remember why I added this...
+dnumberlist = []#cannot remember why I added this...
 
-debug=False
-# print debug messages
 
-files=True
-#number files
-
-folders=True
-#number folders
-
-gitignore=True
-# ignore .git files
-
-sanatizename=True
-sanatationchar='-'
-# cleanup names aaa bbb ccc becomes aaa-bbb-ccc
-
+debug=False # print debug messages
+files=True #number files
+folders=True  #number folders
+gitignore=True # ignore .git files
+searchstr='.git'# or other directories....
+sanatizename=True #cleanup filenames
+sanatationchar='-' # cleanup names aaa bbb ccc becomes aaa-bbb-ccc
 timeout=10.00
-timeouts=True
-#restart loop after timeout
+timeouts=True #restart loop after timeout
+Overwrite=False # in the case of a double number the latter in order gets relocated
+diffnumb=True # count folders,then files, files have own numbering system:
+#diffn=False        True
+#1.1 folder1 vs     1.1
+#1.2 folder2        1.2
+#1.1 file1          1.3
+#1.2 file2          1.4
+
 
 
 def sanatize(s):
-     if (sanatizename==True):
-          s = re.sub(pattern2, '', s)
-          s = re.sub(pattern3, sanatationchar, s)
-     return s
+    print(s)
+    if (sanatizename==True):
+        s = re.sub(pattern2, '', s)
+        s = re.sub(pattern3, sanatationchar, s)
+        print(s)
+    return s
 
 def splitter(var1,var4):
     var3, var2=[], []
     try:
         var2 = var1.split ("_",1) #split on first occuring _
         if (len(var2)<2 or (re.search(pattern1, var2[0])is not None)):
-            var2.insert (0, "001.03") # if it has no number, assign no number
+            var2.insert (0, "001.03") # if it has no number, assign new number
         var3 = map(int, var2[0].split ("."))
         if all(isinstance(x,int) for x in var3):
             if var4 is True:
@@ -59,8 +59,8 @@ def splitter(var1,var4):
             print ("no split possible")
     return()
 
-def changer (currently,tobe):
-    for i in range(len(currently)):
+def changer (current,tobe):
+    for i in range(len(current)):
         try:
             os.rename(current[i], tobe[i])
             if debug==True:
@@ -75,14 +75,15 @@ def changer (currently,tobe):
 
 
 def lecteur(root,basecode,dirlist):
-    newname, dnumberlist, oldpath, var2, var3=[], [], [], [], []
+    newname, dnumberlist, oldpath, var2, var3=[], [0], [], [], []
+    # if lists are large, this will become slow, hence both list and set, http://stackoverflow.com/questions/14667578/check-if-a-number-already-exist-in-a-list-in-python
     for subdir in dirlist:
         var2,var3,var4=splitter(subdir, False)
         var3.pop() #shed last element
         var5=sanatize(var4) #make nice name without losign original
-        if var3==basecode: #if equal to topnumber is equal to root, check for nice name, else continue
+        if (var3==basecode and (var2[1] not in dnumberlist)*(not Overwrite)): #if equal to topnumber is equal to root, check for nice name, else continue
             dnumberlist.append(int(var2[-1]))
-            if var5 == var4:
+            if var5 != var4:
                 newname.append(var5)
                 oldpath.append(os.path.join(root,subdir))               
             else:
@@ -94,35 +95,40 @@ def lecteur(root,basecode,dirlist):
     return (maxd,oldpath,newname)
     
 
-def architect (bcode,maxd,newname):
+def architect (dirname,bcode,maxd,newname):
     var2=[]
     for y in range(len(newname)):
-        var2.append(os.path.join(dirname,'%s.%s_%s' % (bcode, (maxd+y), sanatize(newname[y]))))
+        var2.append(os.path.join(dirname,'%s.%s_%s' % (bcode, (maxd+1+y), newname[y])))
     return(var2)
     
+
 def main(startpath):
-    tobe, oldname, newname=[], [], []
+    tobe, oldname, newname, checklist =[], [], [], []
     maxd = int
     for dirname, subdirList, filenames in os.walk(startpath):
-        if (gitignore==True and ('.git' in dirname)):
-            dirname.remove('.git')
+        if (dirname == startpath or (len(subdirList)==0 and len(filenames)==0)):
+            continue
+        if (gitignore==True and ((searchstr in subdirList) or (searchstr in dirname))):
+            #subdirList.remove('.git')
+            continue
             #don't go into git dirs
-        if (folders==True):
-            if (dirname == startpath or (len(subdirList)==0 and len(filenames)==0)):
-                continue
+        maxd =0
+        if (folders|(files*diffnumb)):
             compbasecode, basecode, var4 = splitter(os.path.basename(dirname), True)
-            maxd,oldname,newname=lecteur(dirname,basecode,subdirList)
-            tobe=architect(compbasecode,maxd,newname)
+            checklist = subdirList * folders + filenames * files * diffnumb
+            maxd,oldname,newname=lecteur(dirname,basecode,checklist)
+            tobe=architect(dirname,compbasecode,maxd,newname)
             changer(oldname,tobe)
-        if (files==True):
             tobe, oldname, newname=[], [], []
-            maxd=0
+            maxd=(maxd * diffnumb)
+        if (files * (not diffnumb)):
             maxd,oldname,newname=lecteur(dirname,basecode,filenames)
-            tobe=architect(compbasecode,maxd,newname)
+            tobe=architect(dirname,compbasecode,maxd,newname)
             changer(oldname,tobe)
-            maxd=0
             tobe, oldname, newname=[], [], []
     return()
+
+
 
 while timeouts:
      clear()
